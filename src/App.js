3fe8d802d673b02c0c3f6eb1c2acf1a8ed000a58
Maksim Aisin <maksim.aisin@mail.ru>
2022-07-05 12:30:43 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import data from './data.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([])
+    const loadData = () => JSON.parse(JSON.stringify(data))
+
+    useEffect(() => {
+        setUsers(loadData)
+    }, [])
+
+    return (
+        <>
+            {users.map((user) => {
+                const { id, name = user.Fullname } = user
+                return <p key={id}>{name}</p>
+            })}
+        </>
+    )
 }
 
-export default App;
+export default App
