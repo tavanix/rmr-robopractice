a9783fb7b12354caa68ae135c1f804e60de91e2b
Maksim Aisin <maksim.aisin@mail.ru>
2022-08-07 20:47:14 +0300

    refactor: remove note

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import Header from './components/Header'
 import UsersDataGrid from './components/UsersDataGrid'
-import Note from './components/Note'
 
 export default function App() {
     return (
